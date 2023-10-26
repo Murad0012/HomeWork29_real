98e3e17f173050a9e1c36ee472df3ec55f470255
Predator <murad.mammedzade11@gmail.com>
2023-10-26 11:19:20 -0700

    sidebar close function added

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ var bar = document.getElementById("icon");
 var sidebar = document.getElementById("jsbar");
 var block1 = document.getElementById("block1");
 var block2 = document.getElementById("block2");
-var block3 = document.getElementById("block3");
 let bool = false
 
 bar.onclick = function(){
     bool = !bool;
     sidebar.style.transform = "translateX(0px)";
+}
+
+block1.onclick = function(){
+    if(bool){
+        sidebar.style.transform = "translateX(-300px)";
+    }
+    bool = !bool;
+}
+
+
+block2.onclick = function(){
+    if(bool){
+        sidebar.style.transform = "translateX(-300px)";
+    }
+    bool = !bool;
+
 }
